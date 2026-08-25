6794862de30ad7d8ac6e9261ef07ae4e56118528
Samuel Alves Medeiros <samuelalvesmedeirosjk@gmail.com>
2026-08-25 18:58:45 -0300

    more studies

--- ficha-funcionario/script.js
+++ ficha-funcionario/script.js
@@ -2,14 +2,19 @@ let data = new Date();
 let hora = data.getHours();
 let greeting;
 let generateRecord = document.getElementById('generateRecord');
-generateRecord.addEventListener('click', createRecord);
+generateRecord.addEventListener('click', createRecord )
 
 function createRecord() {
-    let userName = document.getElementById('userNameTxt').value;
-    let userAge = Number(document.getElementById('userAgeTxt').value);
-    let userPosition = document.getElementById('userPositionTxt').value;
-    let userCountry = document.getElementById('userCountryTxt').value;
+    let userName = document.getElementById('user-name').value;
+    let userAge = document.getElementById('user-birth-date').value;
+    let userPosition = document.getElementById('user-position').value;
+    let userCountry = document.getElementById('user-country').value;
     let result = document.getElementById('resultId');
+
+    let userBirthday = new Date(userAge);
+    let today = data.getFullYear();
+    let idade = today - userBirthday.getFullYear();
+
     if (hora >=0 && hora <=5) {
         greeting = 'Boa madrugada'
     } else if (hora >5 && hora <12) {
@@ -19,7 +24,7 @@ function createRecord() {
     } else if (hora >=18 && hora <=23) {
         greeting = 'Boa noite'
     }
-    result.innerHTML = `${greeting}Idade:${userAge}<br> ${userCountry}<br>` 
+    result.innerHTML = `${greeting}, ${userName}! <br> Idade:${idade}<br> ${userCountry}<br> ${userPosition}` 
 
 }
 
