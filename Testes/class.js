5e717f77dd5152cbafe28933abd14d00ae70ff01
Samuel Alves Medeiros <samuelalvesmedeirosjk@gmail.com>
2026-09-02 10:40:17 -0300

    classes in Js

--- Testes/class.js
+++ Testes/class.js
@@ -1,12 +1,65 @@
-class Carro{
+// class Carro{
 
-    constructor(marca){
-        this.nomeDoCarro = marca
-    }
+//     constructor(marca){
+//         this.nomeDoCarro = marca
+//     }
 
-}
+// }
 
 
-const carro1 = new Carro('Ford')
+// const carro1 = new Carro('Ford')
 
-console.log((carro1.nomeDoCarro))
+// console.log((carro1.nomeDoCarro))
+
+// let nome = 'samuel'
+
+// function ola (){
+//     return `Olá ${nome}`
+// }
+
+// console.log(ola())
+
+
+// class Animal{
+//     constructor (especie, nome, som ){
+//         this.especie = especie
+//         this.nome = nome
+//         this.som = som
+//     }   
+
+//     get fazerSom(){
+//         console.log(this.som)
+//     }
+
+
+//     set cadastratSom(som){
+//         this.som = som
+        
+//     }
+
+// }
+
+// const cachorro = new Animal('Mamifero','gato','miau');
+
+// console.log(cachorro.fazerSom)
+
+// cachorro.cadastratSom = 'au'
+
+// console.log(cachorro.fazerSom)
+
+// class Aluno{
+//     constructor (nome, idade, matricula, cpf){
+        
+//         this.nome = nome
+//         this.idade = idade
+//         this.matricula = matricula
+//         this.cpf = cpf
+
+//     }
+
+// }
+
+// const aluno1 = new Aluno ('Samuel', 19, 202620119, 15741247490)
+
+// console.log(aluno1.nome, aluno1.idade, aluno1.matricula, aluno1.cpf)
+
